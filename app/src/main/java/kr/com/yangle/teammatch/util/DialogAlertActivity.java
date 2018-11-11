aaba76313ab43f43b068d320312420ddae2574ca
maloman72 <maloman72@naver.com>
2018-11-11 23:10:48 +0900

    Bug fixes

--- app/src/main/java/kr/com/yangle/teammatch/util/DialogAlertActivity.java
+++ app/src/main/java/kr/com/yangle/teammatch/util/DialogAlertActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -24,6 +25,7 @@ public class DialogAlertActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.dialog_alert);
 
         mContext = this;
